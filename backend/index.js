f6a0a93c22a8442bf6923b19ea8f866a95cbd52b
Manoj kumar <manojkumardharmavarapu18@gmail.com>
2025-12-25 09:32:42 +0530

    chore: initialize MediaStack backend and frontend setup

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app=express()
+const port=9000
+app.listen(port,()=>{
+    console.log("created");
+    
+})
+app.get("/",(req,res)=>{
+    res.send("done")
+})
